ae552a7fc16d35111c4dd15d0ab27ed3333bd649
Mateus WB <mateus.wolfart@f1solucoes.com.br>
2021-07-12 11:45:51 -0300

    New route about

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => res.send('My first REST API!'));
+app.get('/about', (req, res) => res.send('Im Mat'));
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
